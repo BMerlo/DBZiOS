fa588d3287aa720ef496be7e098096a42bff570a
BorisMAC <bodamelo@gmail.com>
2019-03-15 22:29:17 -0400

    animation idle added
    asdfasd

--- dbz/GameScene.swift
+++ dbz/GameScene.swift
@@ -12,16 +12,14 @@ import AVFoundation
 class GameScene: SKScene {
     
     var music:AVAudioPlayer = AVAudioPlayer()
-    
-    
-    //TODO: - Use this to create a menu scene
     var background:SKSpriteNode! //Sprite
-    var goku = SKSpriteNode()
     let screenSize: CGRect = UIScreen.main.bounds
     
-    var TextureAtlas:[SKTextureAtlas]!
-    var idleAnimation: SKAction?
-    var idleFrames = [SKTexture]()
+    var gokuSprite:SKSpriteNode! //0
+    var gokuIdleAtlas: SKTextureAtlas! //Spritesheet //1
+    var gokuIdleFrames: [SKTexture]! //frames //2
+    var gokuIdle: SKAction! //Animation //3
+    
     
     override init(size: CGSize) {
         super.init(size: size)
@@ -35,33 +33,31 @@ class GameScene: SKScene {
         }
         music.play()
         
-        TextureAtlas = [SKTextureAtlas]()
-        TextureAtlas.append(SKTextureAtlas(named: "idle.1"))
-        
-        var punchFrames1: [SKTexture] = []
-        let numImages0 = TextureAtlas[0].textureNames.count - 1
-            
-        
-        for i in 0...numImages0 {
-            let Name = "idle_\(i).png"
-           // idleFrames.append(SKTexture(imageNamed: Name))
-            idleFrames.append(TextureAtlas[0].textureNamed(Name))
-            
-        }
-        
-        goku = SKSpriteNode(texture: SKTexture(imageNamed: "idle_0"))
-        goku.position = CGPoint(x: screenSize.width/2, y:screenSize.height/2)
-        
-        goku.run(SKAction.repeatForever(SKAction.animate(with: punchFrames1, timePerFrame: 0.2, resize: true, restore: true)))
-        
         background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: screenSize.width/2, y:screenSize.height/2)
         background.size = CGSize(width: screenSize.width, height: screenSize.height)
         
+        //gokuSprite.name = "goku"
+        gokuIdleAtlas = SKTextureAtlas(named: "idle.1") //0
+        
+        gokuIdleFrames = [] //2. Initialize empty texture array
+        
+        let gokuIdleImages = gokuIdleAtlas.textureNames.count-1//3. count how many frames inside atlas (if this does not work do
+        
+        //4. for loop
+        for i in 0...gokuIdleImages {
+            let texture = "Idle_\(i)" //grab each frame in atlas
+            gokuIdleFrames.append(gokuIdleAtlas.textureNamed(texture))
+        }//add frame to texture array
+        gokuIdle = SKAction.animate(with: gokuIdleFrames, timePerFrame: 0.3, resize: true, restore: true)
+       
+        gokuSprite = SKSpriteNode(texture: SKTexture(imageNamed: "idle_0.png"))
+        gokuSprite.position = CGPoint(x: screenSize.width/2-20, y:screenSize.height/2)
+        
         addChild(background)
-        addChild(goku)
+        addChild(gokuSprite)
         
-        
+        gokuSprite.run(SKAction.repeatForever(gokuIdle)) // this way the animation will keep playing for ever
     }
     
     required init?(coder aDecoder: NSCoder) {
